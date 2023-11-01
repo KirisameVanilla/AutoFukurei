749e64bb0d3a0fcc28c5301af30a06b0550daf82
KirisameVanilla <y1147465926@gmail.com>
2023-11-01 12:10:34 +0800

    add func to delete temp pics

--- AutoFukurei.py
+++ AutoFukurei.py
@@ -3,14 +3,18 @@ __author__ = "KirisameVanilla"
 
 from airtest.core.api import *
 from airtest.core.settings import Settings
+import os
 Settings.LOG_DIR = "./"
 
 auto_setup(__file__)
 
 if __name__=='__main__':
+    wait(Template(r"tpl/tpl1698334081332.png", record_pos=(0.221, 0.053), resolution=(1600, 900)))
+    time.sleep(1)
     touch(Template(r"tpl/tpl1698334081332.png", record_pos=(0.221, 0.053), resolution=(1600, 900)))
     time.sleep(1)
     touch(Template(r"tpl/tpl1698334112870.png", record_pos=(0.171, 0.139), resolution=(1600, 900)))
+    time.sleep(1)
     touch(Template(r"tpl/tpl1698334829236.png", record_pos=(-0.079, 0.122), resolution=(1600, 900)),duration=10)
     while True:
         time.sleep(1)
@@ -29,13 +33,15 @@ if __name__=='__main__':
         wait(Template(r"tpl/tpl1698336982880.png", record_pos=(0.393, 0.226), resolution=(1600, 900)))
         time.sleep(1)
         touch(Template(r"tpl/tpl1698336982880.png", record_pos=(0.393, 0.226), resolution=(1600, 900)))
-
         wait(Template(r"tpl/tpl1698336273325.png", record_pos=(0.363, 0.24), resolution=(1600, 900)))
-        assert_not_exists(Template(r"tpl/tpl1698464484711.png", record_pos=(0.339, -0.129), resolution=(1600, 900)), "End")
-        
+        assert_not_exists(Template(r"tpl/tpl1698464484711.png")), "End")
         time.sleep(1)
         touch(Template(r"tpl/tpl1698336273325.png", record_pos=(0.363, 0.24), resolution=(1600, 900)))
         wait(Template(r"tpl/tpl1698335301968.png", record_pos=(0.002, 0.191), resolution=(1600, 900)))
         time.sleep(1)
         touch(Template(r"tpl/tpl1698335301968.png", record_pos=(0.002, 0.191), resolution=(1600, 900)))
-
+        
+        # delete running temp pics
+        temp_pics = [p for p in os.listdir() if p.endswith('.jpg')]
+        for pic in temp_pics:
+            os.remove(pic)
