5ab188f707f5ab6135fa4dcbe1482f8e92c8e364
KirisameVanilla <y1147465926@gmail.com>
2023-11-12 01:37:37 +0800

    Make codes run without AirtestIDE

--- AutoFukurei.py
+++ AutoFukurei.py
@@ -2,38 +2,43 @@
 __author__ = "KirisameVanilla"
 
 from airtest.core.api import *
+from airtest.cli.parser import cli_setup
 from airtest.core.settings import Settings
-import os
-Settings.LOG_DIR = "./"
+
+Settings.THRESHOLD = 0.8
+os.system('adb devices')
+if not cli_setup():
+    auto_setup(devices=[
+        "Android:///emulator-5554?cap_method=javacap&ori_method=adbori",
+    ])
 
 auto_setup(__file__)
 
-if __name__=='__main__':
-    wait(Template(r"tpl/Special.png", record_pos=(0.221, 0.053), resolution=(1600, 900)))
-    time.sleep(1)
-    touch(Template(r"tpl/Special.png", record_pos=(0.221, 0.053), resolution=(1600, 900)))
-    time.sleep(1)
-    touch(Template(r"tpl/Fukurei.png", record_pos=(0.171, 0.139), resolution=(1600, 900)))
+if __name__ == '__main__':
     wait(Template(r"tpl/Challenge.png", record_pos=(0.249, 0.228), resolution=(1600, 900)))
     time.sleep(1)
     if not exists(Template(r"tpl/LevelAddUp.png", record_pos=(-0.079, 0.122), resolution=(1600, 900))):
-        touch((800,200))
+        touch((800, 200))
         time.sleep(1)
 
-    touch(Template(r"tpl/LevelAddUp.png", record_pos=(-0.079, 0.122), resolution=(1600, 900)),duration=10)
+    # Switch to the highest level
+    touch((670, 670), times=2)
+
     while True:
         time.sleep(1)
         wait(Template(r"tpl/Challenge.png", record_pos=(0.249, 0.228), resolution=(1600, 900)))
         time.sleep(1)
-        touch(Template(r"tpl/Challenge.png", record_pos=(0.249, 0.228), resolution=(1600, 900)))
+        # touch Challenge
+        touch((1180, 810))
+        time.sleep(1)
         wait(Template(r"tpl/title.png", record_pos=(0.206, -0.217), resolution=(1600, 900)))
         if not exists(Template(r"tpl/RecordStart.png", record_pos=(0.339, 0.216), resolution=(1600, 900))):
-            touch((1400,700))
+            touch((1400, 700))
             time.sleep(1)
         wait(Template(r"tpl/RecordStart.png", record_pos=(0.339, 0.216), resolution=(1600, 900)))
         time.sleep(1)
-        touch(Template(r"tpl/RecordStart.png", record_pos=(0.339, 0.216), resolution=(1600, 900)))
-        wait(Template(r"tpl/AwdGet.png", record_pos=(-0.003, -0.182), resolution=(1600, 900)),timeout=240)
+        touch((1335, 785))
+        wait(Template(r"tpl/AwdGet.png", record_pos=(-0.003, -0.182), resolution=(1600, 900)), timeout=240)
         time.sleep(1)
         touch(Template(r"tpl/AwdGet.png", record_pos=(-0.003, -0.182), resolution=(1600, 900)))
         wait(Template(r"tpl/OnceAwdGet.png", record_pos=(0.002, -0.179), resolution=(1600, 900)))
@@ -48,7 +53,7 @@ if __name__=='__main__':
         wait(Template(r"tpl/tapScreen.png", record_pos=(0.002, 0.191), resolution=(1600, 900)))
         time.sleep(1)
         touch(Template(r"tpl/tapScreen.png", record_pos=(0.002, 0.191), resolution=(1600, 900)))
-        
+
         # delete running temp pics
         temp_pics = [p for p in os.listdir() if p.endswith('.jpg')]
         for pic in temp_pics:
--- README.md
+++ README.md
@@ -3,14 +3,4 @@
 ### Tutorial
 `git clone git@github.com:KirisameVanilla/AutoFukurei.git`
 
-- Change the name of the folder to 'AutoFukurei.air'
-
-- Download and Install [AirtestIDE](https://airtest.netease.com/)
-
-- Open the folder as a script in AirtestIDE
-
-- Window-Devices-Connect
-
-- Go to your simulator and manually complete one Fukurei battle and save the record, and manually use record mode to complete one Fukurei battle
-- Return to main page of the game
-- Run the script
+- Start from the Fukurei Window
